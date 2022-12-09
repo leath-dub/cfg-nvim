e9296ba7898588a19ff628d5787393a077bc14de
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-12-09 20:58:02 +0000

    got dep working mostly

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,73 +1,47 @@
--- bootstrap packer
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
 function SetTheme(name)
   vim.cmd('silent! colorscheme ' .. name)
 end
 
-return require('packer').startup({function(use)
-
-  use 'wbthomason/packer.nvim'
-
-  use 'rust-lang/rust.vim'
-  use 'LnL7/vim-nix'
-  use {
-    'sainnhe/everforest',
+require "bootstrap"
+require "dep" {
+  {
+    "sainnhe/everforest",
+    function() return SetTheme('everforest') end,
     setup = function()
       vim.g.everforest_better_performance = 1
       vim.g.everforest_background = 'hard'
-    end,
-    config = "SetTheme('everforest')"
-  }
-  use 'aklt/plantuml-syntax'
-  use {
-    'RRethy/vim-hexokinase',
-    run = 'make hexokinase',
-    setup = function()
-      vim.g.Hexokinase_highlighters = {'virtual'}
     end
-  }
-  use {
-    'ThePrimeagen/harpoon',
-    requires = {'nvim-lua/plenary.nvim'}
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    requires = {'tree-sitter/tree-sitter'},
+  },
+  {
+    "RRethy/vim-hexokinase",
     config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'c', 'lua' },
-        sync_install = false,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false
+      os.execute("make hexokinase")
+    end
+  },
+  {
+    "ThePrimeagen/harpoon",
+    requires = "nvim-lua/plenary.nvim"
+  },
+  {
+    "tree-sitter/tree-sitter",
+    deps = {
+      "nvim-treesitter/nvim-treesitter",
+      setup = function()
+        require('nvim-treesitter.configs').setup {
+          ensure_installed = { 'c', 'lua' },
+          sync_install = false,
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false
+          }
         }
-      }
-    end
-  }
-
-  use {
-    'nvim-treesitter/nvim-treesitter-context',
-    requires = {'nvim-treesitter/nvim-treesitter'}
-  }
-
-  -- telescope {{{
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/plenary.nvim'}},
-    config = function()
+      end,
+      deps = "nvim-treesitter/nvim-treesitter-context"
+    }
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    function()
       require('telescope').setup {
         pickers = {
           find_files = {
@@ -75,32 +49,24 @@ return require('packer').startup({function(use)
           }
         }
       }
-    end
-  }
-  use 'nvim-telescope/telescope-fzy-native.nvim'
-  -- }}}
-  -- Language server protocol {{{
-  use 'neovim/nvim-lspconfig'
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = {
-       'hrsh7th/cmp-nvim-lsp',
-       'hrsh7th/cmp-buffer',
-       'hrsh7th/cmp-path',
-       'hrsh7th/cmp-cmdline'
-    },
-    config = function()
-      local cmp = require('cmp')
+    end,
+    requires = "nvim-lua/plenary.nvim",
+    deps = "nvim-telescope/telescope-fzy-native.nvim"
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    function()
+      local cmp = require("cmp")
       if cmp ~= nil then cmp.setup({
         mapping = cmp.mapping.preset.insert({
-          ['<c-y>'] = cmp.mapping.confirm {
+          ["<c-y>"] = cmp.mapping.confirm {
             select = true
           }
         }),
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'buffer', keyword_length = 5 },
-          { name = 'path' }
+          { name = "nvim_lsp" },
+          { name = "buffer", keyword_length = 5 },
+          { name = "path" }
         },
         experimental = {
           native_menu = false,
@@ -108,22 +74,13 @@ return require('packer').startup({function(use)
         }
       })
       end
-    end
+    end,
+    requires = {
+       "neovim/nvim-lspconfig",
+       "hrsh7th/cmp-nvim-lsp",
+       "hrsh7th/cmp-buffer",
+       "hrsh7th/cmp-path",
+       "hrsh7th/cmp-cmdline"
+    }
   }
-  -- }}}
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end,
-config = {
-  display = {
-    open_fn = function()
-      return require('packer.util').float({border = 'single'})
-    end
-  },
-  profile = {
-    enable = true,
-    threshold = 1
-  }
-}})
+}
--- lua/theme.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd([[
-  highlight Comment cterm=italic
-  highlight SpellLocal cterm=Underline ctermbg=NONE
-  highlight IncSearch cterm=NONE ctermfg=red ctermbg=black guibg=#cc241d guifg=#1d2021
-  highlight Search cterm=NONE ctermfg=red ctermbg=black guibg=#cc241d guifg=#1d2021
-]])
