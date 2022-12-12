a139690d04a756da5b79fe14d7908a8faf0abf2e
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-12-12 12:57:57 +0000

    oopsy: fixed deps for tree-sitter, setup was not running

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -24,11 +24,11 @@ require("dep") {
   },
   {
     "tree-sitter/tree-sitter",
-    deps = {
+    deps = {{
       "nvim-treesitter/nvim-treesitter",
-      setup = function()
+      function()
         require('nvim-treesitter.configs').setup {
-          ensure_installed = { 'c', 'lua', 'vim', 'help' },
+          ensure_installed = { 'c', 'lua', 'vim', 'help', 'python' },
           sync_install = false,
           highlight = {
             enable = true,
@@ -37,7 +37,7 @@ require("dep") {
         }
       end,
       deps = "nvim-treesitter/nvim-treesitter-context"
-    }
+    }}
   },
   {
     "nvim-telescope/telescope.nvim",
@@ -79,66 +79,66 @@ require("dep") {
       {
         "neovim/nvim-lspconfig",
         function()
-         local lspconfig = require('lspconfig')
+          local lspconfig = require('lspconfig')
 
-         -- pip install python-lsp-server
-         lspconfig.pylsp.setup {
-           settings = {
-             pylsp = {
-               plugins = {
-                 pycodestyle = {
-                   ignore = {'PEP8'},
-                   maxLineLength = 80
-                 }
-               }
-             }
-           }
-         }
+          -- pip install python-lsp-server
+          lspconfig.pylsp.setup {
+            settings = {
+              pylsp = {
+                plugins = {
+                  pycodestyle = {
+                    ignore = {'PEP8'},
+                    maxLineLength = 80
+                  }
+                }
+              }
+            }
+          }
 
-         lspconfig.rust_analyzer.setup {}
+          lspconfig.rust_analyzer.setup {}
 
-         lspconfig.sumneko_lua.setup {
-           settings = {
-             Lua = {
-               runtime = {
-                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                 version = 'LuaJIT',
-               },
-               diagnostics = {
-                 -- Get the language server to recognize the `vim` global
-                 globals = {'vim'},
-               },
-               workspace = {
-                 -- Make the server aware of Neovim runtime files
-                 library = vim.api.nvim_get_runtime_file("", true),
-               },
-               -- Do not send telemetry data containing a randomized but unique identifier
-               telemetry = {
-                 enable = false,
-               },
-             },
-           }
-         }
+          lspconfig.sumneko_lua.setup {
+            settings = {
+              Lua = {
+                runtime = {
+                  -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                  version = 'LuaJIT',
+                },
+                diagnostics = {
+                  -- Get the language server to recognize the `vim` global
+                  globals = {'vim'},
+                },
+                workspace = {
+                  -- Make the server aware of Neovim runtime files
+                  library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                  enable = false,
+                },
+              },
+            }
+          }
 
-         -- C/C++ lsp
-         lspconfig.ccls.setup {
-           init_options = {
-             compilationDatabaseDirectory = "build";
-             index = {
-               threads = 0;
-             };
-             clang = {
-               excludeArgs = { "-frounding-math"} ;
-             };
-           }
-         }
+          -- C/C++ lsp
+          lspconfig.ccls.setup {
+            init_options = {
+              compilationDatabaseDirectory = "build";
+              index = {
+                threads = 0;
+              };
+              clang = {
+                excludeArgs = { "-frounding-math"} ;
+              };
+            }
+          }
 
-         -- Typescript lsp
-         lspconfig.denols.setup {}
+          -- Typescript lsp
+          lspconfig.denols.setup {}
 
-         -- Go lsp
-         lspconfig.gopls.setup {}
-      end
+          -- Go lsp
+          lspconfig.gopls.setup {}
+        end
       },
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
