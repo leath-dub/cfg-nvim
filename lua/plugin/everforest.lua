96f1929f3e4f0a04c9c3e5919c5e26aedddd73b1
Cathal O'Grady <fierceinbattle@gmail.com>
2023-01-12 17:22:20 +0000

    weird issues with removing packer

--- lua/plugin/everforest.lua
+++ lua/plugin/everforest.lua
@@ -5,6 +5,9 @@ return {
   end,
   setup = function()
     vim.g.everforest_better_performance = 1
+    vim.g.everforest_enable_italic = 1
+    vim.g.everforest_diagnostic_text_highlight = 1
+    vim.g.everforest_diagnostic_line_highlight = 1
     vim.g.everforest_background = 'medium'
   end,
   deps = {
