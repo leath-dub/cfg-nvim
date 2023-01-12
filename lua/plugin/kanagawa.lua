efcede569aeb4db1af37f94524e39d0f6cd3e333
Cathal O'Grady <fierceinbattle@gmail.com>
2023-01-12 20:34:07 +0000

    changes to kanagawa config

--- lua/plugin/kanagawa.lua
+++ lua/plugin/kanagawa.lua
@@ -1,7 +1,29 @@
 return {
   "rebelot/kanagawa.nvim",
   function()
+    local ok, kanagawa = pcall(require, "kanagawa")
+    if not ok then
+      return nil
+    end
+    local ok, colors = pcall(require, "kanagawa.colors")
+    colors = colors.setup()
+    if not ok then
+      return nil
+    end
+    kanagawa.setup {
+      statementStyle = { bold = false },
+      overrides = {
+        Boolean = { bold = false },
+        CursorLineNr = { bold = false },
+        CursorLine = { bg = colors.bg_light0, bold = false },
+        MatchParen = { bold = false },
+        LineNr = { bg = colors.bg_light0 },
+        ["@keyword.operator"] = { bold = false }
+      }
+    }
     vim.cmd("colorscheme kanagawa")
+  end,
+  setup = function()
   end,
   deps = {
     require("plugin.stat")
